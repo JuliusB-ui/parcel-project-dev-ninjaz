a4e708597fda87271af4726ec2bfad0007904911
Itsguzman <roydeguzman1130@gmail.com>
2024-02-04 21:30:26 +0800

    1st commit
    1st commit for catalogv2

---
+++ src/index.js
@@ -0,0 +1,69 @@
+// Catalog Modal here when clicking images
+(() => {
+  const refs = {
+    modalx: document.querySelector('.modal-me'),
+    modalCloseBtn: document.querySelector('.closed-me'),
+    modalTriggers: document.querySelectorAll('.modal-showme'),
+  };
+
+  // Add event listener to close modal button
+  refs.modalCloseBtn.addEventListener('click', closeModal);
+
+  // Add event listeners to each modal trigger (image)
+  refs.modalTriggers.forEach(trigger => {
+    trigger.addEventListener('click', openModal);
+  });
+
+  function openModal() {
+    if (window.innerWidth > 1157) {
+      refs.modalx.classList.remove('is-hidden');
+    }
+  }
+
+  function closeModal() {
+    refs.modal.classList.add('is-hidden');
+  }
+
+  // Prevent modal trigger when clicking certain images
+  var imageTriggers = document.querySelectorAll('.image-trigger');
+  imageTriggers.forEach(function (image) {
+    image.addEventListener('click', function (event) {
+      if (window.innerWidth <= 1157) {
+        event.preventDefault();
+      }
+    });
+  });
+})();
+
+// Catalog Modal here when clicking button
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+// page watches animation here
+window.addEventListener('scroll', function () {
+  var images = document.querySelectorAll('.catalog-img');
+  images.forEach(function (image) {
+    var bounding = image.getBoundingClientRect();
+    if (bounding.top < window.innerHeight) {
+      image.style.animationPlayState = 'running';
+    }
+  });
+});
+
+//modal image selection here
+function displayImage(imageUrl) {
+  var bigImage = document.getElementById('bigImagex');
+  bigImage.src = imageUrl.src;
+}
